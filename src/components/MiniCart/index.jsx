dadb6e3c5f748576ef98f16662bc6bcc5f89c60f
Chan Wan <chanwan@shopplustech.com>
2022-10-16 23:00:21 +0800

    Beautify mini-cart

--- src/components/MiniCart/index.jsx
+++ src/components/MiniCart/index.jsx
@@ -21,51 +21,38 @@ const MiniCart = () => {
 
   return (
     <div className="mini-cart">
-      <ul>
-        {cartContent && cartContent.length ? (
-          cartContent.map((item) => (
-            <li key={item.id} className="mini-cart__item">
-              <div className="mini-cart__item__title">
-                <strong>{item.itemData.name}</strong> × {item.quantity}
-              </div>
-              <div className="mini-cart__item__buttons">
-                <button
-                  type="button"
-                  data-item-id={item.id}
-                  onClick={onAddItem}
-                >
-                  <span className="material-symbols-outlined">
-                    exposure_plus_1
-                  </span>
-                </button>
-                <button
-                  type="button"
-                  data-item-id={item.id}
-                  onClick={onMinusItem}
-                >
-                  <span className="material-symbols-outlined">
-                    exposure_neg_1
-                  </span>
-                </button>
-                <button
-                  type="button"
-                  data-item-id={item.id}
-                  onClick={onRemoveItem}
-                >
-                  <span className="material-symbols-outlined">close</span>
-                </button>
-              </div>
-            </li>
-          ))
-        ) : (
-          <div className="mini-cart__message">
-            <span className="material-symbols-outlined">
-              production_quantity_limits
-            </span>
-            <span>No items added</span>
+      {cartContent && cartContent.length ? (
+        <>
+          <ul>
+            {cartContent.map((item) => (
+              <li key={item.id} className="mini-cart__item">
+                <div className="mini-cart__item__title">
+                  <strong>{item.itemData.name}</strong> × {item.quantity}
+                </div>
+                <div className="mini-cart__item__buttons">
+                  <button type="button" data-item-id={item.id} onClick={onAddItem}>
+                    <span className="material-symbols-outlined">exposure_plus_1</span>
+                  </button>
+                  <button type="button" data-item-id={item.id} onClick={onMinusItem}>
+                    <span className="material-symbols-outlined">exposure_neg_1</span>
+                  </button>
+                  <button type="button" data-item-id={item.id} onClick={onRemoveItem}>
+                    <span className="material-symbols-outlined">close</span>
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className="mini-cart__action">
+            <button type="button">Checkout</button>
           </div>
-        )}
-      </ul>
+        </>
+      ) : (
+        <div className="mini-cart__message">
+          <span className="material-symbols-outlined">production_quantity_limits</span>
+          <span>No items added</span>
+        </div>
+      )}
     </div>
   );
 };
